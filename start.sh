be6fc534c0dba90a9cf2e88033847bc53e372039
kkren <tangjl55@gmail.com>
2017-07-10 21:37:40 +0800

    20170710backup

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-java -Xms100M -Xmx500M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.11.2-b1104.jar
+    #!/bin/sh
+    while((`cat auto`==1))
+    do
+    java -Xms100M -Xmx500M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.11.2-b1104.jar
+    done
